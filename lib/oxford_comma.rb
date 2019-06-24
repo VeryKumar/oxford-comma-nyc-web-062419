94bfc3aeda2e0406b2e4e8450165f695332f9ba5
Varun <uberkumar@gmail.com>
2019-06-24 04:00:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    first_part = ''
+  return "#{array[0..array.length - 2]}, and #{array[-1]66}"
+  end
 end
